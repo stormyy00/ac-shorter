293d9dcdccfc4309b7e4ff915c178a42c09d1f97
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-07-31 01:26:11 -0700

    updated env name

--- frontend/src/utils/auht-client.ts
+++ frontend/src/utils/auht-client.ts
@@ -2,7 +2,7 @@ import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTERAUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [jwtClient()],
 })
 
--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -30,7 +30,7 @@ export const auth = betterAuth({
     },
   },
   plugins: [jwt()],
-  baseURL: process.env.BETTERAUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 });
 
 
